f8e239eefe8ccab4a8e6bbbde9e110bea7a525ec
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-03 14:37:52 -0300

    agrego ej8

---
+++ js/ej8.js
@@ -0,0 +1,19 @@
+let numeroDeRepeticiones = prompt("Número de repeticiones:");
+
+if (Number(numeroDeRepeticiones) == numeroDeRepeticiones) {
+  if (numeroDeRepeticiones > 0 && numeroDeRepeticiones <= 50) {
+    let rep, i;
+
+    for (i = 0; i <= numeroDeRepeticiones; i++) {
+      for (rep = 1; rep <= i; rep++) {
+        document.write(rep);
+      }
+      document.write("<br>");
+    }
+  } else
+    document.write(
+      "<p>El número no es válido, ingrese un número hasta el 50</p>"
+    );
+} else {
+  document.write("<p>Ingrese un número, no letra</p>");
+}
