1cb535e7ef527784c90d106d741a55dbdce64627
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-03 10:09:55 -0300

    agrego ej1

--- js/ej1.js
+++ js/ej1.js
@@ -1,1 +1,12 @@
+const edad = prompt('Ingrese su edad:');
+const edadNumero = parseInt(edad);
 
+if (isNaN(edadNumero)) {
+    document.write('<p>La edad ingresada no es un número válido.</p>');
+} else {
+  if (edadNumero > 18) {
+    document.write('<p>Ya puede conducir.</p>');
+  } else {
+    document.write('<p>No tiene la edad necesaria para conducir.</p>');
+  }
+}
